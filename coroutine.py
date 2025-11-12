640c52134c10953484aac10ebb7a01fef6be8209
sora19961102 <sora11023539@gmail.com>
2025-11-12 14:32:21 -0800

    Enhance coroutine functionality by adding an ID parameter to fetch_data and implementing multiple concurrent fetches in main.

--- coroutine.py
+++ coroutine.py
@@ -1,17 +1,29 @@
 import asyncio
 
-async def fetch_data(delay):
-    print(f"Fetching data after {delay} seconds")
+# Define a coroutine that simulates a time-consuming task
+async def fetch_data(delay, id):
+    print(f"Fetching data after {delay} seconds for {id}")
+    # Simulate an I/O operation with a sleep
     await asyncio.sleep(delay)
-    print("Data fetched")
-    return f"Data fetched after {delay} seconds"
+    print(f"Data fetched for {id}")
 
-# coroutine function
+    return f"Data fetched after {delay} seconds for {id}"
+
+# Define another coroutine that uses the first coroutine
 async def main():
     print("Start of main coroutine")
-    task = fetch_data(2)
+
+    task = fetch_data(2, 1)
+    task2 = fetch_data(2, 2)
+
+    # Await the fetch_data coroutine, pausing execution of main until fetch_data completes
     result = await task
     print(f"Received result: {result}")
+
+    result2 = await task2
+    print(f"Received result: {result2}")
+
     print("End of main coroutine")
 
+# Run the main coroutine
 asyncio.run(main())
